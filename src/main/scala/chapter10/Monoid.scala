0ae3c8a5219c12c19be72724cf0d458affd499a8
Filipponi, Luca <luca.filipponi89@gmail.com>
2018-04-14 17:31:08 +0100

    continued chapter 10

--- src/main/scala/chapter10/Monoid.scala
+++ src/main/scala/chapter10/Monoid.scala
@@ -61,9 +61,9 @@ object Monoid {
   }
 
   def optionMonoid[A]: Monoid[Option[A]] = new Monoid[Option[A]] {
-    override def op(a1: Option[A], a2: Option[A]): Option[A] = ???
+    override def op(a1: Option[A], a2: Option[A]): Option[A] = a1.orElse(a2)
 
-    override def zero: Option[A] = ???
+    override def zero: Option[A] = None
   }
 
 
--- src/test/scala/chapter10/MonoidTest.scala
+++ src/test/scala/chapter10/MonoidTest.scala
@@ -1,32 +1,47 @@
 package chapter10
 
-import chapter10.Monoid.{stringMonoid,listMonoid}
+import chapter10.Monoid.{stringMonoid, listMonoid}
 import org.scalatest.{FlatSpec, Matchers}
 
 class MonoidTest extends FlatSpec with Matchers {
 
-  "stringMonoid" should "combine two elememt" in {
+  trait StringFixture {
+    val (first, second, third) = ("a", "b", "c")
+  }
 
-    stringMonoid.op("Hello ","There") should be("Hello There")
+  trait ListFixture {
+    val (first, second, third) = (List(1), List(2), List(3))
+  }
+
+  "stringMonoid" should "combine two string with the op operator" in new StringFixture {
+
+    stringMonoid.op(first, second) should be(s"$first$second")
+  }
+
+  it should "respect the associativity law for op" in new StringFixture {
+
+    //(a + b) + c = a + (b + c)
+    val leftSide = stringMonoid.op(stringMonoid.op(first, second), third)
+    val rightSide = stringMonoid.op(first, stringMonoid.op(second, third))
+    leftSide should be(rightSide)
 
   }
 
-  "stringMonoid" should "leave the element as the same when combined with the zero element" in {
+  it should "leave the element as the same when combined with the zero element" in new StringFixture {
 
-    stringMonoid.op("Hello",stringMonoid.zero) should be("Hello")
-
-  }
-
-  "listMonoid" should "combine two string" in {
-
-    listMonoid.op(List("Hello"),List("There")) should be(List("Hello","There"))
+    val elem = "a"
+    stringMonoid.op(elem, stringMonoid.zero) should be(elem)
 
   }
 
-  "listMonoid" should "leave the element as the same when combined with the zero element" in {
+  /**
+    * I should thing a bit more about how test the monad instance:
+    *
+    * 1) I have to test the operation per se?
+    * 2) For sure i need to test the associativity rule
+    * 3) I need to the test the validity of the zero element
+    */
 
-    listMonoid.op(List("Hello"),listMonoid.zero) should be(List("Hello"))
 
-  }
 
 }
