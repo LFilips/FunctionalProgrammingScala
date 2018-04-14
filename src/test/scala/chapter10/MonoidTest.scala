688b2e123d8a7e282878877e53971835f3a53eaf
Filipponi, Luca <luca.filipponi89@gmail.com>
2018-04-14 18:36:20 +0100

    improved monoid testing

--- src/test/scala/chapter10/MonoidTest.scala
+++ src/test/scala/chapter10/MonoidTest.scala
@@ -5,43 +5,84 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class MonoidTest extends FlatSpec with Matchers {
 
-  trait StringFixture {
-    val (first, second, third) = ("a", "b", "c")
+  trait MonoidFixture[A] {
+    val tuple: (A, A, A)
+    val first : A
+    val second : A
+    val third : A
   }
 
-  trait ListFixture {
-    val (first, second, third) = (List(1), List(2), List(3))
+  trait StringFixture extends MonoidFixture[String] {
+    val tuple = ("a", "b", "c")
+    val first = tuple._1
+    val second = tuple._1
+    val third = tuple._1
   }
 
-  "stringMonoid" should "combine two string with the op operator" in new StringFixture {
-
-    stringMonoid.op(first, second) should be(s"$first$second")
-  }
-
-  it should "respect the associativity law for op" in new StringFixture {
-
-    //(a + b) + c = a + (b + c)
-    val leftSide = stringMonoid.op(stringMonoid.op(first, second), third)
-    val rightSide = stringMonoid.op(first, stringMonoid.op(second, third))
-    leftSide should be(rightSide)
-
-  }
-
-  it should "leave the element as the same when combined with the zero element" in new StringFixture {
-
-    val elem = "a"
-    stringMonoid.op(elem, stringMonoid.zero) should be(elem)
-
+  trait ListFixture extends MonoidFixture[List[Int]] {
+    val tuple = (List(1), List(2), List(3))
+    val first = tuple._1
+    val second = tuple._1
+    val third = tuple._1
   }
 
   /**
-    * I should thing a bit more about how test the monad instance:
-    *
-    * 1) I have to test the operation per se?
-    * 2) For sure i need to test the associativity rule
-    * 3) I need to the test the validity of the zero element
+    * Is a list of tuple, where I provide the instance of the monoid to test,
+    * 3 element of the type of the monoid
+    * the operation that the monoid perform
     */
 
+  /** FIXME make it more generic
+    * val monoidList = List(
+    * (stringMonoid, new StringFixture {}, (a: String, b: String) => a + b),
+    * (listMonoid[Int] _, new ListFixture {}, (a: List[Int], b: List[Int]) => a ++ b)
+    * )
+    */
+
+
+  "StringMonoid" should "have op, zero and respect associativity" in {
+
+    val tuple = (stringMonoid, new StringFixture {}, (a: String, b: String) => a + b)
+
+    val monoidUnderTest = tuple._1
+    val monoidFixture = tuple._2
+    val operator = tuple._3
+
+    //testing the operation
+    monoidUnderTest.op(monoidFixture.first, monoidFixture.second) should be(operator(monoidFixture.first, monoidFixture.second))
+
+    //testing associativity rules
+    val leftSide = monoidUnderTest.op(monoidUnderTest.op(monoidFixture.first, monoidFixture.second), monoidFixture.third)
+    val rightSide = monoidUnderTest.op(monoidFixture.first, monoidUnderTest.op(monoidFixture.second, monoidFixture.third))
+    leftSide should be(rightSide)
+
+    //testing zero value combination
+    monoidUnderTest.op(monoidFixture.first, monoidUnderTest.zero) should be(monoidFixture.first)
+    monoidUnderTest.op(monoidUnderTest.zero, monoidFixture.first) should be(monoidFixture.first)
+  }
+
+  "ListMonoid" should "have op, zero and respect associativity" in {
+
+    val listIntMonoid = listMonoid[Int]
+
+    val tuple = (listIntMonoid, new ListFixture {}, (a: List[Int], b: List[Int]) => a ++ b)
+
+    val monoidUnderTest = tuple._1
+    val monoidFixture = tuple._2
+    val operator = tuple._3
+
+    //testing the operation
+    monoidUnderTest.op(monoidFixture.first, monoidFixture.second) should be(operator(monoidFixture.first, monoidFixture.second))
+
+    //testing associativity rules
+    val leftSide = monoidUnderTest.op(monoidUnderTest.op(monoidFixture.first, monoidFixture.second), monoidFixture.third)
+    val rightSide = monoidUnderTest.op(monoidFixture.first, monoidUnderTest.op(monoidFixture.second, monoidFixture.third))
+    leftSide should be(rightSide)
+
+    //testing zero value combination
+    monoidUnderTest.op(monoidFixture.first, monoidUnderTest.zero) should be(monoidFixture.first)
+    monoidUnderTest.op(monoidUnderTest.zero, monoidFixture.first) should be(monoidFixture.first)
+  }
 
 
 }
